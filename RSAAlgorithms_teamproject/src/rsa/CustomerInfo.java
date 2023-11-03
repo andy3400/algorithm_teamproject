ed45d9e893033585afe6e8f01d8bb0aada4e7f6d
Gwanyong Kim <ttole0718@gmail.com>
2023-11-03 17:12:06 +0900

    add comment

--- RSAAlgorithms_teamproject/src/rsa/CustomerInfo.java
+++ RSAAlgorithms_teamproject/src/rsa/CustomerInfo.java
@@ -1,40 +1,29 @@
 package rsa;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CustomerInfo {
     private String name;
-    private int index;
     private String phoneNumber;
     private String address;
 
-    public CustomerInfo(String name, int index, String phoneNumber, String address) {
+    public CustomerInfo(String name, String phoneNumber, String address) {
         this.name = name;
-        this.index = index;
         this.phoneNumber = phoneNumber;
         this.address = address;
-    }
-
+    }//name, phonenumber, address의 변수들 설정 및 초기화 
+    
 
 	public String getName() {
         return name;
-    }
-
-    public int getIndex() {
-        return index;
-    }
+    }//name getter함수
 
     public String getPhoneNumber() {
         return phoneNumber;
-    }
+    }//phonenumber getter함수
+    
     public String address() {
     	return address;
-    }
+    }//address getter함수
     
-    static void staticMethod() {
-    	
-    }
-
-    // 다른 고객 정보 관련 메서드를 추가할 수 있음
+    
+    //CustomerInfo, getter함수 추가 후 고객 추가 정보 사용가능
 }
--- RSAAlgorithms_teamproject/src/rsa/rsa.java
+++ RSAAlgorithms_teamproject/src/rsa/rsa.java
@@ -8,10 +8,10 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class rsa implements MyEncryption {
 	// 수정 테스트
-	//interface
 	/*
 	 * 공개키와 개인키 한 쌍 생성
 	 */
@@ -95,19 +95,33 @@ public class rsa implements MyEncryption {
 
 		return decryptedText;
 	}
-	
+	public int getIndexOfName(String customerName, List<CustomerInfo> customerList) {
+	    for (int i = 0; i < customerList.size(); i++) {
+	        if (customerList.get(i).getName().equals(customerName)) {
+	            return i; // 이름이 일치하는 고객을 찾으면 해당 인덱스를 반환
+	        }
+	    }
+	    return -1; // 이름이 일치하는 고객을 찾지 못한 경우 -1을 반환하거나 다른 값을 사용할 수 있습니다.
+	}
 
 	public static void main(String[] args) {
-		MyEncryption enc = new rsa();
-		List<CustomerInfo> custls = new ArrayList<>();
+		MyEncryption enc = new rsa(); //non static 에러 방지
+		List<CustomerInfo> custls = new ArrayList<>();	//고객 정보 리스트 생성
+		Scanner sc = new Scanner(System.in);
+        rsa a = new rsa();//non static 에러 방지
+       
+
 		
-		custls.add(new CustomerInfo("김관용",1 ,"" ,"" ));
-        custls.add(new CustomerInfo("이태호",2,"",""));
-        custls.add(new CustomerInfo("정은섭",3 ,"" ,"" ));
-        custls.add(new CustomerInfo("박성은",4,"",""));
+		custls.add(new CustomerInfo("김관용","",""));	//("이름", "전화번호", "주소")
+        custls.add(new CustomerInfo("이태호","",""));
+        custls.add(new CustomerInfo("정은섭","",""));
+        custls.add(new CustomerInfo("박성은","",""));
         
+        System.out.println("암호화 할 사람의 이름을 입력하세요: ");
+        String s1 = sc.next(); //찾고자 하는 사람의 이름을 입력
+        int ind = (a.getIndexOfName(s1, custls)+1); //custls의 인덱스 + 1 해서 암호화, 복호화 중 곱,나누기에서 0으로 인한 에러 방지
+		//System.out.println(ind);	//index확인
         
-		
 		HashMap<String, String> rsaKeyPair = enc.createKeypairAsString();
 		String publicKey = rsaKeyPair.get("publicKey");
 		String privateKey = rsaKeyPair.get("privateKey");
